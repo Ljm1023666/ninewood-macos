41d4d91d18d6962d4084eac17d445ec551bb7274
罗建明 <1961786978@qq.com>
2026-07-15 22:06:37 +0800

    Initial Commit

---
+++ ninewood-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ninewood-macos
+//
+//  Created by 罗建明 on 2026/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ninewood-macos/ninewood_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ninewood_macosApp.swift
+//  ninewood-macos
+//
+//  Created by 罗建明 on 2026/7/15.
+//
+
+import SwiftUI
+
+@main
+struct ninewood_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
